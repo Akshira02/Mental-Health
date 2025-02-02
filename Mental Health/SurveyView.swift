59f423727cfde871170926b15aa4df7c0ea423b6
ashtheflash1212 <ashwincharles05@gmail.com>
2025-02-01 18:07:16 -0800

    2/1

---
+++ Mental Health/SurveyView.swift
@@ -0,0 +1,18 @@
+//
+//  SurveyView.swift
+//  Mental Health
+//
+//  Created by Ashwin Charles on 2/1/25.
+//
+
+import SwiftUI
+
+struct SurveyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SurveyView()
+}
